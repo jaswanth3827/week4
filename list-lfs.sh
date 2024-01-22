3f0961b007a7f87b67f6cdceeea4fed7388bdc1d
jaswanth <jaswanthgutta@gmail.com>
2024-01-22 16:49:02 -0700

    update-file

--- list-lfs.sh
+++ list-lfs.sh
@@ -1,9 +1,9 @@
-# Iterate through remote branches
+# List all files greater than 50MB in all remote branches
 for branch in $(git branch -r | grep 'origin/' | sed 's/origin\///'); do
-    # List all files greater than 50MB
-    git rev-list --objects --all | git cat-file --batch-check='%(objectsize) %(rest)' | awk -v branch="$branch" '$1 > 50000000 {print $1, branch, $2}' | while read -r size branch file; do
-        # Check if the file exists in the branch
-        if git ls-tree -r "$branch" "$file" >/dev/null 2>&1; then
+    git ls-tree -r --name-only "$branch" \
+    | while read -r file; do
+        size=$(git cat-file -s "$branch:$file")
+        if [ "$size" -gt 50000000 ]; then
             echo "$size $branch $file"
         fi
     done
